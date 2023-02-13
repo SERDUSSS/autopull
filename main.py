1bf9d87dbb89588eae0c1a820235e835b216a16c
SERDUSSS <serdusone@gmail.com>
2023-02-13 19:40:47 +0100

    rand

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from uuid import uuid4
 
 def main():
+    w = ""
     open('generated.txt','w').write("")
-    w += str(uuid4())
-    w += str(uuid4())
-    w += str(uuid4())
+    w += str(uuid4()) + "\n"
+    w += str(uuid4()) + "\n"
+    w += str(uuid4()) + "\n"
     open('generated.txt', 'a').write(w)
     
 if __name__ == "__main__":

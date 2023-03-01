ee3c4f7919f41dd7f0b51a02d0ecc856b2a0b17d
SERDUSSS <serdusone@gmail.com>
2023-03-01 17:14:24 +0100

    auto

--- run.sh
+++ run.sh
@@ -2,9 +2,9 @@ counter = 1
 
 while [$h -le $u]
 do
-	git add * -q
-	git commit -am auto -q
-	git push > -q
+	git add * >/dev/null 2>&1
+	git commit -am auto >/dev/null 2>&1
+	git push >/dev/null 2>&1
 	echo $counter
 	counter = $counter + 1
 done
